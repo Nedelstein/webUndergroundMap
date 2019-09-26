8421952294b4b0495290dbd62973b909c5c9c2d7
Noah Edelstein <Noedelstein@gmail.com>
2019-09-26 19:21:21 -0400

    changed background img

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,47 @@
 const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: {
+    bundle: "./src/app.js"
+  },
   output: {
     path: path.resolve(__dirname, "../public/assets"),
     filename: "bundle.js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              bypassOnDebug: true,
+              disable: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+  ]
 };
--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 // import styles from "./styles/loader.scss";
+import style from './styles/loader.scss';
+import BackgroundImg from "./assets/subwaymap_background_new-01.png";
+
+document.body.style.backgroundImage = BackgroundImg;
+
 
 setTimeout(refresh, 15000);
 
