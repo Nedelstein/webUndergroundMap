1eb18fc711ad665163824e76a4de7da49c97eec9
Noah Edelstein <Noedelstein@gmail.com>
2019-10-03 15:23:29 -0400

    added phases 14,15

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import style from './styles/loader.scss';
 // document.body.style.backgroundImage = BackgroundImg;
 
 
-setTimeout(refresh, 15000);
+// setTimeout(refresh, 15000);
 
-function refresh() {
-  location.reload(true);
-}
+// function refresh() {
+//   location.reload(true);
+// }
