7dcbc241d59cc1157ab41c92601aa337a4783def
Noah Edelstein <Noedelstein@gmail.com>
2019-10-10 19:05:11 -0400

    removed setTimout function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import styles from './styles/loader.scss';
-// require('file-loader?name=[name].[ext]!./index.html');
-// document.body.style.backgroundImage = BackgroundImg;
 
-setTimeout(refresh, 15000);
-function refresh() {
-    location.reload(true);
-}
+// setTimeout(refresh, 15000);
+// function refresh() {
+//     location.reload(true);
+// }
