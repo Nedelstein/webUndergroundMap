23df9738a41fa79342900faf60c0eb7c8d3c9cd8
Noah Edelstein <Noedelstein@gmail.com>
2019-10-11 00:34:16 -0400

    reset refresh function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import styles from './styles/loader.scss';
 
-// setTimeout(refresh, 15000);
-// function refresh() {
-//     location.reload(true);
-// }
+setTimeout(refresh, 15000);
+function refresh() {
+    location.reload(true);
+}
+
+
